ce58d3623282b32d5a2f1818e6696cce6cfaf1ef
jhcha <hinunbi@barunmo.com>
2013-10-11 14:16:35 +0900

    블로그 게시

---
+++ src/main/java/camel/example/hello/ToConsoleWithoutCamel.java
@@ -0,0 +1,10 @@
+package camel.example.hello;
+
+import java.util.Date;
+
+public class ToConsoleWithoutCamel {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world! at " + new Date());
+	}
+}
